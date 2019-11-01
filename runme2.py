29e77d0fff0657ec5e7e942921258290a4db48d4
ramcdougal <robert.mcdougal@yale.edu>
2019-11-01 16:48:35 -0400

    NEURON style and Python 3 updates

--- runme2.py
+++ runme2.py
@@ -3,8 +3,8 @@
 try:
     from neuron import h, gui
 except:
-    print 'This simulation requires NEURON with Python support.'
-    print 'See: http://neuron.yale.edu'
+    print('This simulation requires NEURON with Python support.')
+    print('See: http://neuron.yale.edu')
     import sys
     sys.exit()
 
@@ -40,13 +40,13 @@ ps.exec_menu('Show Diam')
 h.graphList[0].append(ps)
 
 # initialize simulation
-h.finitialize()
+h.finitialize(-65)
 
 # setup current pulses to trigger APs
 fire_times = [0, 15, 23, 31]
 iclamps = []
 for time in fire_times:
-    iclamp = h.IClamp(0.5, sec=h.soma[0])
+    iclamp = h.IClamp(h.soma[0](0.5))
     iclamp.delay = time
     iclamp.amp = 2
     iclamp.dur = 0.5
@@ -94,7 +94,7 @@ while h.t < stop_time:
     if advance_count % save_every == 0:
         values.append([seg.v for seg in segs])
     advance_count += 1
-    print h.t
+    print(h.t)
 
 #
 # now fatten everything up (so we can see it) and save the graphics
@@ -104,17 +104,17 @@ scale = 5
 import numpy
 for sec in h.allsec():
     if 'soma' not in sec.name():
-        for i in xrange(int(h.n3d(sec=sec))):
-            d = h.diam3d(i, sec=sec)
-            h.pt3dchange(i, d * scale, sec=sec)
+        for i in range(sec.n3d()):
+            d = sec.diam3d(i)
+            sec.pt3dchange(i, d * scale)
     elif 'soma' in sec.name():
         x, y, z, diam = [], [], [], []
-        for i in xrange(int(h.n3d(sec=sec))):
-            x.append(h.x3d(i, sec=sec))
-            y.append(h.y3d(i, sec=sec))
-            z.append(h.z3d(i, sec=sec))
-            diam.append(h.diam3d(i, sec=sec))
-        h.pt3dclear(sec=sec)
+        for i in range(sec.n3d()):
+            x.append(sec.x3d(i))
+            y.append(sec.y3d(i))
+            z.append(sec.z3d(i))
+            diam.append(sec.diam3d(i))
+        sec.pt3dclear()
         x, y, z, diam = scale * numpy.array(x), scale * numpy.array(y), scale * numpy.array(z), scale * numpy.array(diam)
         i = int(len(x) / 2)
         midptx, midpty, midptz = x[i], y[i], z[i]
@@ -122,12 +122,12 @@ for sec in h.allsec():
         y -= midpty / 2.
         z -= midptz / 2.
         for xpt, ypt, zpt, diampt in zip(x, y, z, diam):
-            h.pt3dadd(xpt, ypt, zpt, diampt, sec=sec)
+            sec.pt3dadd(xpt, ypt, zpt, diampt)
 
 
 
 # begin with a full rotation for 40 frames
-for i in xrange(40):
+for i in range(40):
     ps.rotate(0, 0, 0, 0, 6.283 / 40., 0)
     h.doNotify()
     savefig()
@@ -140,7 +140,7 @@ for i, value in enumerate(values):
         ps.rotate(0, 0, 0, 0, 6.283 / 40., 0)
     h.doNotify()
     savefig()
-    print 'i = %d' % i
+    print('i = %d' % i)
 
 # convert the pictures to a video
 os.system('rm -f movie.mp4')
